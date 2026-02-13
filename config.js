1dc1fe3119492cb85c608e08953c6aef4ba8db36
Hnsk23 <hnsk237788@gmail.com>
2026-02-13 12:18:57 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://https://github.com/nirmanh12/HANSA-MD/blob/main/images/HANSA%20md.png?raw=true",
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 HANSA-MD Is Alive Now😍*",
+BOT_OWNER: '94768546691',  // Replace with the owner's phone number
+
+
+
+};
